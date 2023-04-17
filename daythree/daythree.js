f6cdfc2c42a8fcc7ce98924e61c4d2e283f6e72b
RasikaWaghchoure <97951481+RasikaWaghchoure@users.noreply.github.com>
2023-04-17 14:55:18 +0530

    first commit

--- daythree/daythree.js
+++ daythree/daythree.js
@@ -1,4 +1,4 @@
-alert("congratulations")
+/*alert("congratulations")
 alert("hello")
 alert(3+4)
 
@@ -10,6 +10,40 @@ const no=20;
 no=122;
     alert(no)
 
-let a=13;
+/*    let a=13;
     a*=5;
-alert(a)
+alert(a)*/
+
+/*var x=13;
+var y=15;
+alert(x==y && x>y)
+
+if(x>y)
+{
+    alert("Rasika")
+}
+else
+{
+    alert("Waghchoure")
+}
+
+alert(typeof(true))
+
+alert(typeof("Rasika"))
+
+let age=prompt("What is your age");
+    alert("your age is "+age)*/
+
+    let img = document.createElement("img");
+    img.src = "https://picsum.photos/200/301";
+
+    img.style.border = "10px solid orange";
+img.style.borderRadius = "10px";
+
+document.body.appendChild(img);
+
+/*function myFunction("https://picsum.photos/200/301") {
+    return "https://picsum.photos/200/301";
+  }
+
+  document.getElementById("demo").innerHTML = myFunction("https://picsum.photos/200/301");*/
